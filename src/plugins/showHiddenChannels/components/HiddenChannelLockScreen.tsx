3e6e2072aec7ed29201e22aa516263658ae1956d
SenorLawyer <130395604+SenorLawyer@users.noreply.github.com>
2026-09-06 05:29:11 +0200

    Reuse shared channel enums in the hidden-channel screen

--- src/plugins/showHiddenChannels/components/HiddenChannelLockScreen.tsx
+++ src/plugins/showHiddenChannels/components/HiddenChannelLockScreen.tsx
@@ -25,6 +25,7 @@ import { sortPermissionOverwrites } from "@plugins/permissionsViewer/utils";
 import { classes } from "@utils/misc";
 import { formatDuration } from "@utils/text";
 import type { Channel, RoleOrUserPermission } from "@vencord/discord-types";
+import { ChannelFlags, ChannelType as ChannelTypes, ForumLayout as ForumLayoutTypes, VideoQualityMode as VideoQualityModes } from "@vencord/discord-types/enums";
 import { findByPropsLazy, findComponentByCodeLazy, findCssClassesLazy } from "@webpack";
 import { EmojiStore, FluxDispatcher, GuildMemberStore, GuildStore, Parser, PermissionsBits, PermissionStore, SnowflakeUtils, Timestamp, Tooltip, useEffect, useState } from "@webpack/common";
 import { ComponentType } from "react";
@@ -36,33 +37,9 @@ const enum SortOrderTypes {
     CREATION_DATE = 1
 }
 
-const enum ForumLayoutTypes {
-    DEFAULT = 0,
-    LIST = 1,
-    GRID = 2
-}
-
 interface DefaultReaction {
     emojiId: string | null;
     emojiName: string | null;
-}
-
-const enum ChannelTypes {
-    GUILD_TEXT = 0,
-    GUILD_VOICE = 2,
-    GUILD_ANNOUNCEMENT = 5,
-    GUILD_STAGE_VOICE = 13,
-    GUILD_FORUM = 15
-}
-
-const enum VideoQualityModes {
-    AUTO = 1,
-    FULL = 2
-}
-
-const enum ChannelFlags {
-    PINNED = 1 << 1,
-    REQUIRE_TAG = 1 << 4
 }
 
 const ChatScrollClasses = findCssClassesLazy("auto", "managedReactiveScroller", "customTheme");

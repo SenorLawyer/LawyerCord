d9d52779b526164010c3233e636470a1a4a78621
SenorLawyer <130395604+SenorLawyer@users.noreply.github.com>
2026-09-06 11:59:42 +0200

    Remove unused global-only pronoun lookup option

--- src/plugins/userMessagesPronouns/utils.ts
+++ src/plugins/userMessagesPronouns/utils.ts
@@ -21,15 +21,14 @@ import { UserProfileStore, useStateFromStores } from "@webpack/common";
 
 import { PronounsFormat, settings } from "./settings";
 
-function useDiscordPronouns(id: string, useGlobalProfile: boolean = false): string | undefined {
+function useDiscordPronouns(id: string): string | undefined {
     const globalPronouns: string | undefined = useStateFromStores([UserProfileStore], () => UserProfileStore.getUserProfile(id)?.pronouns);
     const guildPronouns: string | undefined = useStateFromStores([UserProfileStore], () => UserProfileStore.getGuildMemberProfile(id, getCurrentChannel()?.getGuildId())?.pronouns);
 
-    if (useGlobalProfile) return globalPronouns;
     return guildPronouns || globalPronouns;
 }
 
-export function useFormattedPronouns(id: string, useGlobalProfile: boolean = false) {
-    const pronouns = useDiscordPronouns(id, useGlobalProfile)?.trim().replace(/\n+/g, "");
+export function useFormattedPronouns(id: string) {
+    const pronouns = useDiscordPronouns(id)?.trim().replace(/\n+/g, "");
     return settings.store.pronounsFormat === PronounsFormat.Lowercase ? pronouns?.toLowerCase() : pronouns;
 }

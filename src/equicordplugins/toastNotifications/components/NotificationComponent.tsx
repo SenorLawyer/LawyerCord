26fd35d146365e03e48577507deedf54df5114f0
SenorLawyer <130395604+SenorLawyer@users.noreply.github.com>
2026-09-06 23:10:54 +0200

    Label toast dismiss buttons without duplicate IDs

--- src/equicordplugins/toastNotifications/components/NotificationComponent.tsx
+++ src/equicordplugins/toastNotifications/components/NotificationComponent.tsx
@@ -10,7 +10,7 @@ import ErrorBoundary from "@components/ErrorBoundary";
 import { settings as PluginSettings } from "@equicordplugins/toastNotifications/index";
 import { classNameFactory } from "@utils/css";
 import { findComponentByCodeLazy } from "@webpack";
-import { FluxDispatcher, GuildStore, IconUtils, React, useEffect, useMemo, useState } from "@webpack/common";
+import { FluxDispatcher, GuildStore, IconUtils, React, useEffect, useState } from "@webpack/common";
 
 import { MessageNotification, NotificationData } from "./Notifications";
 
@@ -108,8 +108,10 @@ export default ErrorBoundary.wrap(function NotificationComponent(props: Notifica
         props.onClose();
     };
 
-    const closeButton = useMemo(() => (
+    const closeButton = (
         <button
+            type="button"
+            aria-label="Dismiss notification"
             className={cl("notification-close-btn")}
             onClick={e => {
                 e.preventDefault();
@@ -117,12 +119,11 @@ export default ErrorBoundary.wrap(function NotificationComponent(props: Notifica
                 props.onClose();
             }}
         >
-            <svg width="24" height="24" viewBox="0 0 24 24" role="img" aria-labelledby="vc-toast-notifications-dismiss-title">
-                <title id="vc-toast-notifications-dismiss-title">Dismiss Notification</title>
+            <svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true">
                 <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z" />
             </svg>
         </button>
-    ), [props.onClose]);
+    );
 
     let content: React.ReactNode;
     if (isMessageNotification(props)) {

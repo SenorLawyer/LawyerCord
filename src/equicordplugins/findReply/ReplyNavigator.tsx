3befc44db261a83651cca689ea38caf0d95318fb
SenorLawyer <130395604+SenorLawyer@users.noreply.github.com>
2026-09-12 06:17:57 +0200

    Remove FindReply duplicate stylesheet import

--- src/equicordplugins/findReply/ReplyNavigator.tsx
+++ src/equicordplugins/findReply/ReplyNavigator.tsx
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import "./styles.css";
-
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Paginator, requirePaginator } from "@plugins/reviewDB/components/ReviewModal";
 import { Message } from "@vencord/discord-types";
